dd0e7316881c087426e303d03fc1736eee8c0b46
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-10-28 20:40:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 touristSpots.sort((a,b)=>{
-	return a-b;
+	if(a>b){
+		return 1;
+	}
+	if(a<b){
+		return -1;
+	}
+	return 0;
 });
 console.log(touristSpots);
 
