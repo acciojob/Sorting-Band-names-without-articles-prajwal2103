a4a26d9f53f7d4344866d4e63595920cee41dac0
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-10-30 18:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 touristSpots.sort((a,b)=>{
-	if(a>b){
-		return 1;
-	}
-	if(a<b){
-		return -1;
-	}
-	return 0;
+	let withoutarticle=[];
+let map={};
+let regexp=/ban\b|\bthe\b|\ba\b\gi;
+for(let i = 0 ; i < touristSpots.length ; i++ ){
+	String str= str.replace(regexp,"");
+	str=str.replace(/ +\g, " ");
+	map[str]=touristSpots[i];
+	withoutarticle.push(map[str])
+}
+withoutarticle.sort();
+for(let i = 0 ; i < withoutarticle.length ; i++){
+	console.log(map[withoutarticle[i]]);
+}
+
 });
-console.log(touristSpots);
 
